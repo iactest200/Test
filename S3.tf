2cf576dd0c9fc2ea758c1255892401ed58b27d12
iactest200 <112950030+iactest200@users.noreply.github.com>
2022-09-06 18:10:11 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -1,11 +1,33 @@
-resource "aws_s3_bucket" "unrestricted" {
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+ # required_version = ">= 0.14.9"
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+ #text
+
+resource "aws_s3_bucket" "app_server12" {
   bucket = "examplebuckettftest"
   acl    = "public-read-write"
   versioning {
     enabled = true
+    
   }
   logging {
-    target_bucket = aws_s3_bucket.log_bucket.id
+    target_bucket = "pavantestbucket"
     target_prefix = "log/"
     }
 }
+
+
+
